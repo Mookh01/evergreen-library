82d36deb4e5e1d6f25e81017fe4a4d2f5487cec6
keith <kmoore@intxlog.com>
2019-04-26 14:46:09 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
-import './App.css';
-import Buttons from './Buttons'
+import{ Button } from 'evergreen-ui'
 import styles from './styles.module.scss'
-import './assets/styles/base.module.scss'
-
 
 class App extends Component {
   render() {
     return (
       <div className={styles.container}>
-          <Buttons></Buttons>
+         <Button appearance="primary" className={styles.button} >Button</Button>
       </div>
     );
   }
