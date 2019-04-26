cb44313c00847f93bc325c4af3cf9aec8cf36843
keith <kmoore@intxlog.com>
2019-04-26 14:09:34 -0400

    removed all components

--- src/App.js
+++ src/App.js
@@ -1,41 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-import {BrowserRouter as Router } from 'react-router-dom'
-import { Route} from 'react-router-dom'
-//import containers
-import LayOut from './Layout'
 import Buttons from './Buttons'
-import LoadSummary from './LoadSummary'
-import LoadSummaryContainer from './LoadSummaryContainer'
-
-//import components
-import BasicPageWithHeader from './BasicPageWithHeader'
 import styles from './styles.module.scss'
 import './assets/styles/base.module.scss'
 
 
 class App extends Component {
-
   render() {
-
     return (
       <div className={styles.container}>
-      <div className="App">
-        <header className="App-header">
-          <BasicPageWithHeader
-          >
-          </BasicPageWithHeader>
-          </header>
-          <Router>
-            <Route path='/layout' component={LayOut}/>
-            <Route path='/buttons' component={Buttons} />
-            <Route path='/login'  />
-            <Route path='/loadsummary' component={LoadSummary} />
-            <Route path='/search' />
-            <Route path='/groups'  />
-            <Route path='/' component={LoadSummaryContainer} />
-          </Router>
-      </div>
+          <Buttons></Buttons>
       </div>
     );
   }
--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -6,18 +6,7 @@ export default class Buttons extends Component {
   render() {
     return (
       <div>
-        <div>
-          <Button marginRight={16} intent="none">None</Button>
           <Button marginRight={16} intent="success">Success</Button>
-          <Button marginRight={16} intent="warning">Warning</Button>
-          <Button marginRight={16} intent="danger">Danger</Button>
-        </div>
-        <div>
-          <Button marginRight={16} appearance="primary" intent="none">None</Button>
-          <Button marginRight={16} appearance="primary" intent="success">Success</Button>
-          <Button marginRight={16} appearance="primary" intent="warning">Warning</Button>
-          <Button marginRight={16} appearance="primary" intent="danger">Danger</Button>
-        </div>
       </div>
     )
   }
